cdab1c529d67b98fbec5da81b92b3abcddacdd1a
Temur Komilov <itteamdev74@gmail.com>
2022-10-12 20:32:02 +0500

    main files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "react-slideshow-image/dist/styles.css";
+import { Slide } from "react-slideshow-image";
+import { image1, image2, image3, image4 } from "./images/images";
 
 function App() {
+  const slideImages = [
+    {
+      url: image1,
+      caption: "Slide 1",
+    },
+    {
+      url: image2,
+      caption: "Slide 2",
+    },
+    {
+      url: image3,
+      caption: "Slide 3",
+    },
+    {
+      url: image4,
+      caption: "Slide 4",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="slide-container">
+        <Slide>
+          {slideImages.map((image, index) => (
+            <div className="each-fade" key={index}>
+              <div
+                className="image-container"
+                style={{
+                  backgroundImage: `url(${image.url})`,
+                  height: "500px",
+                  width: "400px",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center"
+                }}
+              >
+                <span>{image.caption}</span>
+              </div>
+            </div>
+          ))}
+        </Slide>
+      </div>
     </div>
   );
 }
